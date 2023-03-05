1aa132ed03922df63a1d38c1a6deb654b883212d
Onur <onurkordogan@gmail.com>
2023-03-05 23:14:06 +0300

    update

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import Header from '../components/header/Header';
-import Footer from '../components/footer/Footer';
-import { GlobalProvider } from '../contexts/GlobalState';
-import { Box } from '@mui/material';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import { GlobalProvider } from "../contexts/GlobalState";
+import { Box } from "@mui/material";
 
 function Layout() {
-
-    return (
-        <GlobalProvider>
-            <Box
-                sx={{
-                    minHeight: "100vh",
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-between",
-                }}
-            >
-                <Header />
-                <Outlet />
-                <Footer />
-            </Box>
-        </GlobalProvider>
-    )
+  return (
+    <GlobalProvider>
+      <Box
+        sx={{
+          minHeight: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Header />
+        <Outlet />
+        <Footer />
+      </Box>
+    </GlobalProvider>
+  );
 }
 
-export default Layout
+export default Layout;
